8c66ba89b4248e3616bbc0ba48a75d7f15aef9e2
Dewey-Ding <deweyding@gmail.com>
2019-11-06 21:13:35 +0800

    init

---
+++ src/main/java/com/example/springcloudconfigrepo/SpringCloudConfigRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconfigrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigRepoApplication.class, args);
+    }
+
+}
